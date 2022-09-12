f60d0b95038caf7ce89d44e0c87d2188afcfd22e
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-09-12 14:29:22 +0200

    Add firebase to project

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const HomeScreen = () => {
+    const [data, setData] = useState<any>('');
+
+    const gettingData = async () => {
+        const data = await firestore().collection('test').get();
+        data.forEach(snap => console.log(snap.data()));
+    };
+
+    gettingData();
+
     return (
         <View style={styles.container}>
             <Text>Home!</Text>
