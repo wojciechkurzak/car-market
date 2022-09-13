73c111fbc20055e85bd817cd2ab46da3206c1767
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-09-13 17:06:56 +0200

    Add interface for car data

---
+++ interfaces/CarsInterface.ts
@@ -0,0 +1,14 @@
+export interface CarType {
+    id?: string;
+    title?: string;
+    description?: string;
+    mileage?: string;
+    engine?: string;
+    productionDate?: string;
+    price?: string;
+    image?: string;
+    town?: string;
+    country?: string;
+    email?: string;
+    phone?: string;
+}
