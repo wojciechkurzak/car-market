88bf0fe01ba79a52c196dc3e5d206cc7a7b7c032
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-10-21 20:14:38 +0200

    Add auth verification to my cars tab

--- screens/MyCarsScreen.tsx
+++ screens/MyCarsScreen.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {AuthContext} from '../utils/AuthContext';
+import AuthButton from '../components/AuthButton';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {StackParamList} from '../App';
+
+type AuthPanelNavigationProp = StackNavigationProp<StackParamList, 'Auth'>;
 
 const MyCarsScreen = () => {
+    const navigation = useNavigation<AuthPanelNavigationProp>();
+
+    const user = useContext(AuthContext);
+
     return (
-        <View style={styles.container}>
-            <Text>My cars!</Text>
-        </View>
+        <>
+            {user ? (
+                <View style={styles.container}>
+                    <Text>My cars!</Text>
+                </View>
+            ) : (
+                <View style={styles.container}>
+                    <Text style={styles.authStatus}>You are not logged in</Text>
+                    <AuthButton
+                        name="Log in"
+                        access={() => navigation.navigate('Auth')}
+                    />
+                </View>
+            )}
+        </>
     );
 };
 
@@ -14,6 +37,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    authStatus: {
+        alignSelf: 'center',
+        fontSize: 18,
+        marginTop: 20,
     },
 });
 
