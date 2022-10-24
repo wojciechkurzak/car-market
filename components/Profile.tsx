c6f5c664e010fb72ef4669c1d9d7323dcd125c11
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-10-24 17:26:56 +0200

    Add display user image in profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,16 +1,44 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {AuthContext} from '../utils/AuthContext';
+import storage from '@react-native-firebase/storage';
 
 const Profile = () => {
+    const [userImageUrl, setUserImageUrl] = useState<string>('');
+
     const user = useContext(AuthContext);
+
+    console.log(user);
+
+    const downloadUserImage = async (): Promise<void> => {
+        if (user === null || user.photoURL === null) return;
+        const imageRef = storage().ref(
+            `usersImages/${user.uid}/${user.photoURL}`,
+        );
+        const url = await imageRef.getDownloadURL().catch(error => {
+            throw error;
+        });
+        setUserImageUrl(url);
+    };
+
+    useEffect(() => {
+        downloadUserImage();
+    }, []);
 
     return (
         <View style={styles.container}>
-            <Image
-                style={styles.profilePicture}
-                source={require('../assets/defaultIcon.png')}
-            />
+            {userImageUrl.length !== 0 ? (
+                <Image
+                    style={styles.profilePicture}
+                    source={{uri: userImageUrl}}
+                />
+            ) : (
+                <Image
+                    style={styles.profilePicture}
+                    source={require('../assets/defaultIcon.png')}
+                />
+            )}
+
             <Text style={styles.profileName}>{user?.displayName}</Text>
         </View>
     );
